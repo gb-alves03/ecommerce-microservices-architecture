418eba00382dd2f9ce2bc61774a3f2417d3f01df
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-09-28 02:56:51 -0300

    feat: creation of the UserDetailsServiceImpl class with its method

---
+++ authentication/src/main/java/br/com/alves/ecommerce/authentication/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.alves.ecommerce.authentication.service;
+
+import br.com.alves.ecommerce.authentication.domain.User;
+import br.com.alves.ecommerce.authentication.domain.UserDetailsImpl;
+import br.com.alves.ecommerce.authentication.repository.IUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    public UserDetailsServiceImpl(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("User not found."));
+        return new UserDetailsImpl(user);
+    }
+}
